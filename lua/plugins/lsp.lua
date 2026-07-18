f785f379785fbbe40344ac1629bf585ad97ce21e
Jorge Paravicini <24812622+jorgeparavicini@users.noreply.github.com>
2026-07-18 10:48:35 +0200

    chore: roll back snacks layer to rebuild by hand

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,12 +6,6 @@ return {
     "saghen/blink.cmp",
   },
   config = function()
-    vim.diagnostic.config({
-      virtual_lines = { current_line = true },
-      underline = true,
-      severity_sort = true,
-    })
-
     vim.lsp.config("lua_ls", {
       settings = { Lua = { diagnostics = { globals = { "vim" } } } },
     })
@@ -21,11 +15,11 @@ return {
         local map = function(keys, fn, desc)
           vim.keymap.set("n", keys, fn, { buffer = event.buf, desc = "LSP: " .. desc })
         end
-        -- gd / gr are provided by snacks.nvim (picker-based, better in big repos).
+        map("gd", vim.lsp.buf.definition, "Go to definition")
+        map("gr", vim.lsp.buf.references, "Go to references")
         map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
         map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-        -- <leader>e is the snacks explorer, so diagnostics-float lives on <leader>cd.
-        map("<leader>cd", vim.diagnostic.open_float, "Line diagnostics")
+        map("<leader>e", vim.diagnostic.open_float, "Line diagnostics")
       end,
     })
 
